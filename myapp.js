7a685ac008342aaf350c97189559d54b0795f512
Padamati Gopi <89209446+GopiDeveloper@users.noreply.github.com>
2022-02-27 08:45:52 +0530

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -200,6 +200,79 @@ app.delete('/deleteOrders',(req,res) =>{
 
 
 
+app.get('/category',(req, res) => {
+    db.collection('productitem').find().toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/products',(req, res) => {
+    db.collection('productname').find().toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/product/:id',(req,res) => {
+    var id = parseInt(req.params.id);
+    db.collection('productname').find({"product_id":id}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+app.get('/filter/:itemid',(req, res) =>{
+    var id = parseInt(req.params.itemid);
+    db.collection('productname').find({"categoryType.categorytype_id":id}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+app.get('/menu',(req, res) => {
+    db.collection('productmenu').find().toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/menu/:proid',(req,res) => {
+    var proid = Number(req.params.proid);
+    db.collection('productmenu').find({"product_id":proid}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.post('/menuItem',(req,res) => {
+    console.log(req.body);
+    db.collection('productmenu').find({menu_id:{$in:req.body}}).toArray((err,result) =>{
+        if(err) throw err; 
+        res.send(result)
+    })
+    
+})
+
+
+app.post('/placeorde',(req,res) => {
+    console.log(req.body);
+    db.collection('productorders').insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.send("order placed")
+    })
+})
+
+app.get('/orde',(req, res) => {
+    db.collection('productorders').find().toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+
+
 //connecting with mongodb
 MongoClient.connect(mongoUrl, (err,client)=>{
     if(err) console.log("Error while connecting")
